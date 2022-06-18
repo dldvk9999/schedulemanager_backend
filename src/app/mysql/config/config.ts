22d135f89f4204fab014df4f4264281385d0c957
dldvk9999 <47880131+dldvk9999@users.noreply.github.com>
2022-06-18 18:28:44 +0900

    Update config.ts

--- src/app/mysql/config/config.ts
+++ src/app/mysql/config/config.ts
@@ -1,8 +1,8 @@
 module.exports = {
-    host: 'localhost',
-    username: 'root',
-    password: 'whdrms6533@',
-    db: 'schedulemanager',
+    host: 'm7az7525jg6ygibs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    username: 'a5j7p1yarp1j1bcv',
+    password: 'xuutnwx6vwar6ut3@',
+    db: 'lzyjdz4ochz3j61y',
     dialect: 'mysql',
     pool: {
         max: 5,
